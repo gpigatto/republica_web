2d1f1385caadcc97d0d998d05d818ba20870b6a8
Gabriel Pigatto <gabrielpigatto@live.com>
2020-10-15 21:08:34 -0300

    Update firebaseConfig.js

--- src/core/firebaseConfig.js
+++ src/core/firebaseConfig.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyB6pv-Ms6dSe-jzWDwMjGnE2HinjvTswv4",
-  authDomain: "republica-a360c.firebaseapp.com",
-  databaseURL: "https://republica-a360c.firebaseio.com",
-  projectId: "republica-a360c",
-  storageBucket: "republica-a360c.appspot.com",
-  messagingSenderId: "332356986662",
-  appId: "1:332356986662:web:4b84e89d94bc225c6043e7",
-  measurementId: "G-YGYL4QV8C2"
+  ****
 };
 
 firebase.initializeApp(config);
